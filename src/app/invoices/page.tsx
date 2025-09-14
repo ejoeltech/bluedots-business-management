a6104c9b14a664dfd95dd16b809b87728533dd72
ejoeltech-4856 <ejoeltech@gmail.com>
2025-09-14 18:02:45 +0100

    Fix syntax error in invoices page - removed extra closing div tag that was causing build failure

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -454,7 +454,6 @@ function InvoicesPageContent() {
                         </select>
                       </div>
                     </div>
-                  </div>
                   
             <div className="flex justify-end space-x-3 mt-6">
               <button
